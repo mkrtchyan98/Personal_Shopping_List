e442bd19cfa34a316edd6c0e6058e87fec68704e
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2020-07-04 23:44:25 +0400

    creat global context

---
+++ src/containers/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Route, Switch } from 'react-router-dom';
+import ListsContextProvider,{ListsContext} from '../Context/ListsContextProvider';
+import ItemsContextProvider,{ItemsContext} from '../Context/ItemsContextProvider';
+import Header from '../components/Header/Header';
+import Lists from './Lists';
+import List from './List';
+import Form from './Form';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppWrapper>
+      <Header />
+      <ListsContextProvider>
+      <ItemsContextProvider>
+      <ListsContext.Consumer>
+      {({list,lists,loading:listsLoading,error:listsError,getListsRequest,getListRequest }) => (
+        <ItemsContext.Consumer>
+       { ({ items,loading:itemsLoading,error:itemsError,getItemsRequest,addItemRequest}) => (
+
+      <Switch>
+       <Route exact path='/' 
+       render={props => lists && <Lists lists={lists} loading={listsLoading} error={listsError} getListsRequest={getListsRequest} {...props} />} />
+        <Route path='/list/:id/new'render={props => <Form addItemRequest={addItemRequest} {...props} />} />
+        <Route path='/list/:id'
+         render={props => list  && items && <List list={list} items={items} loading={itemsLoading} error={itemsError} getListRequest={getListRequest} getItemsRequest={getItemsRequest} {...props} />}/>
+      </Switch>
+      )}
+              </ItemsContext.Consumer>
+)}
+      </ListsContext.Consumer>
+            </ItemsContextProvider>
+
+            </ListsContextProvider>
+
+    </AppWrapper>
+  </>
+);
+
+export default App;
---
+++ src/containers/List.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import SubHeader from '../components/Header/SubHeader';
+import ListItem from '../components/ListItem/ListItem';
+
+const ListItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
+
+const Alert = styled.span`
+  width: 100%;
+  text-align: center;
+`;
+    const List = ({items,loading , error,list, getListRequest, getItemsRequest, match,history}) => {
+
+   React.useEffect(() => {
+    if(!list.id ) {
+      getListRequest(match.params.id);
+    }
+    if(!items.length > 0 ) {
+      getItemsRequest(match.params.id)
+    }
+
+   },[items,list,match.params.id,getItemsRequest,getListRequest])
+  return !loading && !error ? (
+    <>
+      {history && list &&
+       (
+        <SubHeader
+          goBack={() => history.goBack()}
+          openForm={() => history.push(`${match.url}/new`)}
+        />
+      )}
+      <ListItemWrapper>
+        {items && items.map(item => <ListItem key={item.id} data={item} />)}
+      </ListItemWrapper>
+    </>
+  ) : (
+    <Alert>{loading ? 'Loading...' : error}</Alert>
+  );
+};
+
+
+export default List
